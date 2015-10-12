7004c6d494e399b569ca00503df786bacfd0d3e8
chriszeng8 <chriszeng8@hotmail.com>
2015-10-11 23:36:45 -0400

    version 1 of data prep complete

--- data_prep.R
+++ data_prep.R
@@ -10,6 +10,13 @@ require(graphics)
 library(RCurl) # download https data
 library(data.table)
 
+## NOTE: the part where we may try something different will be marked with comment as follows:
+## ** TODO: try option 1: @#$#@!@# (done)
+##          try option 2: @#$#@!@# (to do)
+##          try option 3: @#$#@!@# (to do)
+
+## Just just search for the key word: TODO to jump to the place where we would like to try out different options
+
 rm(list=ls())
 setwd("/Users/apple1/Github_Repo/Kaggle/SpringLeaf_Marketing")
 
@@ -31,7 +38,6 @@ train[, ID := NULL]; test[, ID := NULL]
 
 # Feature names (ID removed already)
 feature.names <- setdiff(names(train),"target")
-
 # Extract names of non-numeric features
 feature.types <- lapply(train,class)
 nonnumeric.types <- feature.types[(feature.types!="integer")&(feature.types!="numeric")]
@@ -83,37 +89,53 @@ date.features <-c("VAR_0073","VAR_0075","VAR_0156","VAR_0157","VAR_0158","VAR_01
                   "VAR_0178","VAR_0179","VAR_0204","VAR_0217")
 print (train[,(date.features),with=F])
 
+## ======================== PROCESSING DATE FEATURES ============================
+## ** TODO: try option 1: convert date time into continuous numeric value (done)
+##          try option 2: convert date with 00:00 into mday, month, year 3 columns,
+##                        and date with specific time (i.e., non-midnight time)
+##                        mday, month, year, hour 4 different columns     (to do)
+##          try option 3: based on either option 1 or option 2, add weekday boolean 
+##                        as additional feature                           (to do)
 
-######### MAY break these columns into date/time two columns
+## IMPlEMENTED OPTION 1 (begin)
 for (f in date.features) {
-     #     as.Date(train[[f]],"%d%b%Y")
      # Currently convert date time
      train[[f]] <- as.double(strptime(train[[f]], format='%d%b%y:%H:%M:%S', tz="UTC"))
+     test[[f]] <- as.double(strptime(test[[f]], format='%d%b%y:%H:%M:%S', tz="UTC"))
 }
+print (train[,(date.features), with = F])
+## IMPlEMENTED OPTION 1 (end)
 
-print (train[,(date.features),with =F])
-# Extract Hours for the columns with non-zero
-
+##  OPTION 2 - TO DO (begin)
+############ Option 2: Extract Hours for the columns with non-00:00:00 time #############
+##
+##
+##  OPTION 2 - TO DO (end)
 
 print("remaining non-date variables:")
 print(setdiff(diverse.features, date.features))
 # [1] "VAR_0200" "VAR_0237" "VAR_0274" "VAR_0342" "VAR_0404" "VAR_0493"
 
+## ======================== PROCESSING CITIES FEATURES ============================
+## ** TODO: try option 1: only keep levels with frequency > 1000 (done)
+##              option 2: only keep levels with frequency > 800 (to do)
+##              option 2: only keep levels with frequency > 500 (to do)
+
 # FEATURE: VAR_0200 individual cities
 print(paste('number of different cities:',length(table(train[, VAR_0200]))))
-
 # decide what levels/cities to keep for VAR_0200
 VAR_0200.freq <- table(train[, VAR_0200])
 print (VAR_0200.freq[VAR_0200.freq>train.size*0.005]) # returns CHICAGO
+
+################ Parameter here: city cut off criteria 1000, 800, 500 ################
+## IMPlEMENTED OPTION 1 (begin)
 VAR_0200.freqlevels <- names(VAR_0200.freq[VAR_0200.freq>1000])
-print (train[,VAR_0200]) # [1] "CHICAGO"      "HOUSTON"      "JACKSONVILLE" "SAN ANTONIO" 
+## IMPlEMENTED OPTION 1 (end)
+
+print (train[,VAR_0200.freqlevels]) # [1] "CHICAGO"      "HOUSTON"      "JACKSONVILLE" "SAN ANTONIO" 
 replaced.VAR_0200col <- ifelse(!train[,VAR_0200]%in%VAR_0200.freqlevels,-1,train[,VAR_0200])
 replaced.VAR_0200col_test <- ifelse(!test[,VAR_0200]%in%VAR_0200.freqlevels,-1,test[,VAR_0200])
-print (sum(!is.na(replaced.VAR_0200col))/train.size) # only 3.76% are not NA, so may be we won't keep this feature
-###!!!!!!!!!!!!!!!!! MAY EXCLUDE THIS FEATURE/and compare performance !!!!!!!!!!!!!!!!!!!###
-######################## MAY map the city to its state ########################
-length(train[,VAR_0200])
-length(replaced.VAR_0200col)
+#print (sum(!(replaced.VAR_0200col=="-1"))/train.size) # only 3.76% are not NA, so may be we won't keep this feature, will see
 train[ , VAR_0200 := replaced.VAR_0200col]
 test[ , VAR_0200 := replaced.VAR_0200col_test]
 
@@ -121,29 +143,32 @@ test[ , VAR_0200 := replaced.VAR_0200col_test]
 # FEATURE: VAR_0237 - states
 # FEATURE: VAR_0274 - states
 # FEATURE: VAR_0342 - states
-table(train[,VAR_0237])
-table(train[,VAR_0274])
-table(train[,VAR_0342])
-length(table(train[,VAR_0237])) # level "" + level of states 48
-length(table(train[,VAR_0274])) # level -1 + level "" + level of states 56
-length(table(train[,VAR_0342])) # level -1 + level "" + level of states 49
-# Create 
+state.features <- c("VAR_0237","VAR_0274","VAR_0342")
+# table(train[,VAR_0237])
+# table(train[,VAR_0274])
+# table(train[,VAR_0342])
+# length(table(train[,VAR_0237])) # level "" + level of states 48
+# length(table(train[,VAR_0274])) # level -1 + level "" + level of states 56
+# length(table(train[,VAR_0342])) # level -1 + level "" + level of states 49
+
+# replace "" in state with -1
 train[,VAR_0237 := (ifelse(train[,VAR_0237]=="",-1,train[,VAR_0237]))]
 train[,VAR_0274 := (ifelse(train[,VAR_0274]=="",-1,train[,VAR_0274]))]
 train[,VAR_0342 := (ifelse(train[,VAR_0342]=="",-1,train[,VAR_0342]))]
-
-state.features <- c("VAR_0237","VAR_0274","VAR_0342")
+test[,VAR_0237 := (ifelse(test[,VAR_0237]=="",-1,test[,VAR_0237]))]
+test[,VAR_0274 := (ifelse(test[,VAR_0274]=="",-1,test[,VAR_0274]))]
+test[,VAR_0342 := (ifelse(test[,VAR_0342]=="",-1,test[,VAR_0342]))]
 train[, (state.features), with = F]
 
 # REMAINING: [1] "VAR_0404" "VAR_0493"
 # two types: with job title and without job title
+jobtitle.features<-c("VAR_0404","VAR_0493")
 VAR_0404col<-train[,VAR_0404]
 sum(VAR_0404col==-1)+sum(VAR_0404col=="") # without job titles
 replaced.VAR_0404col<-ifelse( ((VAR_0404col==-1)|(VAR_0404col=="")), "NoJobDesc", "JobDesc")
 replaced.VAR_0404col_test<-ifelse( ((test[,VAR_0404]==-1)|(test[,VAR_0404]=="")), "NoJobDesc", "JobDesc")
 train[ , VAR_0404 := replaced.VAR_0404col]
 test[ , VAR_0404 := replaced.VAR_0404col_test]
-
 VAR_0493col<-train[,VAR_0493]
 table(train[,VAR_0493col])
 sum(VAR_0493col==-1)+sum(VAR_0493col=="") # without job titles
@@ -151,16 +176,18 @@ replaced.VAR_0493col<-ifelse( ((VAR_0493col==-1)|(VAR_0493col=="")), "NoJobDesc"
 replaced.VAR_0493col_test <-ifelse( ((test[,VAR_0493]==-1)|(test[,VAR_0493]=="")), "NoJobDesc", "JobDesc")
 train[ , VAR_0493 := replaced.VAR_0493col]
 test[ , VAR_0493 := replaced.VAR_0493col_test]
-
-
-jobtitle.features<-c("VAR_0404","VAR_0493")
 train[, (jobtitle.features), with = F]
 
 # VAR_0044 should be removed
 train[, VAR_0044 := NULL]
 test[, VAR_0044 := NULL]
 
-# a critical step!
+## ===================== REPLACING MISSING NUMERIC DATA  =========================
+## ** TODO: try option 1: replace with -1
+##          try option 2: replace with median
+##          try option 3: replace with nearest neighbor or K nearest neighbor
+
+## IMPlEMENTED OPTION 1 (begin)
 cat("replacing missing values with -1\n")
 for (f in setdiff(names(train),"target")) {
      if (class(train[[f]])!="character") {
@@ -170,40 +197,101 @@ for (f in setdiff(names(train),"target")) {
           test[, (f):= temp_test_col]
      }
 }
+## IMPlEMENTED OPTION 1 (end)
 
-# convert to factor
+# convert character variable to factor/categorical
 for (f in setdiff(names(train),"target")) {
      if (class(train[[f]])=="character") {
           train[[f]] = as.factor(train[[f]])
      }
 }
 
-
 # Now missing data are all filled.
 sum(is.na(train)) # 0
 sum(is.na(test)) # 0
 
-
-
-# AFTER CLEANING THE DATA, CHECK DATA AGAIN
+# ======= CHECK DATA AGAIN (for checking purpose only) ============
 feature.types<-lapply(train,class)
 nonnumeric.types<-feature.types[(feature.types!="integer")&(feature.types!="numeric")]
 nonnumeric.featurenames<-names(nonnumeric.types)
 train[,(nonnumeric.featurenames),with=F]
-
 
 factornum_df <- data.frame(matrix(NA,nrow=1,ncol=length(nonnumeric.featurenames)))
 names(factornum_df) <-nonnumeric.featurenames
 for (feature in nonnumeric.featurenames) {
      factornum_df[1,(feature)] <- length(table(train[,(feature),with=F]))
 }
-
-##### convert all non numeric features to 
+# make sure only three type of classes "integer", "factor", "numeric" exist
 feature.types<-lapply(train,class)
 length(feature.types)
 sum(feature.types=="integer")+sum(feature.types=="factor")+sum(feature.types=="numeric")
 
+# ==== convert data table to data frame, simply because I want to get something done quickly, 
+# ==== and I'm a lot more comfortable with data.frame.
+train<-data.frame(train)
+test<-data.frame(test)
+
+# ============= replace "-1" and "" in categorical data with label "other"  ==================
+for (i in names(train)) {
+     if (class(train[,i])=="factor") {
+          temp = as.character(train[,i])
+          temp = ifelse(((temp=="")|(temp=="-1")),"other",temp)
+          train[,i]=as.factor(temp)
+     }
+}
+
+# ================== for categorical variables with only two levels, =========================
+# === remove it if the single dominant level makes up over 99.5% (close to zero variance) ====
+nearZeroVarFeatures<-c()
+for (i in names(train)) {
+     if (class(train[,i])=="factor") {
+          print(paste("========= factor:",i,"============")) 
+          temp = as.character(train[,i])
+          print (unique(temp))
+          print (table(temp))
+          # if only 2 variables, look at ratio minus label count/total label count
+          if (length(unique(temp))==2) {
+               if (min(table(temp))/nrow(train)*100<0.5)
+              nearZeroVarFeatures<-c(nearZeroVarFeatures,i)
+          }
+     }
+}
+# remove these near zero variance categorical features
+train<-train[,setdiff(names(train),nearZeroVarFeatures)]
+test<-test[,setdiff(names(test),nearZeroVarFeatures)]
+
+######### There is also a way to remove zero variance numeric features #########
+### a function called nearZeroVar in 
+# library(caret)
+# print (paste('Column count before cutoff:',ncol(train)))
+# nzv <- nearZeroVar(train, saveMetrics = TRUE)
+# head(nzv,100) 
+# #dim(nzv[nzv$percentUnique>0.1,])
+# percentUniqueThres=0.05
+# selectedFeatures<-rownames(nzv[nzv$percentUnique>percentUniqueThres,])
+# train<-train[,selectedFeatures]
 
 
+########============= The most important step!!! creating dummy variables =======########
+#Create dummy variable (with base level removed)/One-hot encoding
+for (f in names(train)) {
+     if (class(train[,f])=="factor") {
+          alllevels= as.character(unique(train[,f]))
+          levelsWithoutBase = alllevels[(1:(length(alllevels)-1))]
+          #           print (paste("====== ",f,"======"))
+          #           print (alllevels)
+          #           print (levelsWithoutBase)
+          for (level in levelsWithoutBase) {
+               train[paste(f,level,sep="_")]<-ifelse(train[,f]==level,1,0)
+               test[paste(f,level,sep="_")]<-ifelse(test[,f]==level,1,0)
+          }
+     }
+}
+# Remove categorical data now since dummy variable are all created 
+factorFeatures<-names(train)[unlist(lapply(train,class))=="factor"]
+train<-train[,setdiff(names(train),factorFeatures)]
+test<-test[,setdiff(names(test),factorFeatures)]
 
-save(train,test,file="train_test_df.RData")
+# save(train,test,testID,file="processed_train_test.RData")
+rm(list=setdiff(ls(),c("test","train","testID")))
+save(train,test,testID,file="Processed_Data.RData")
