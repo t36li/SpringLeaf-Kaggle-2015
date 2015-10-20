5912e106a821fb6956b8067d87892dfd1d0854ee
t36li <bob.tianbo@gmail.com>
2015-10-20 01:14:28 -0400

    Update with cv code (Work in progress)

--- model_run.R
+++ model_run.R
@@ -1,47 +1,64 @@
 require(xgboost)
-library(caret)
-setwd("C:/Users/bli13/Documents/Kaggle")
+require(data.table)
 rm(list=ls())
-#source("data_prep.R")
-load("Processed_Data.RData")
+setwd("C:/Users/bli13/Documents/Kaggle")
 
-# Perform LASSO to select features to reduce dimensionality and resolve collinearity
-control <- trainControl(method="repeatedcv", number=5, repeats=5)
-# train the model
-model <- train(x=data.matrix(train[,setdiff(names(train),'target')]), 
-               y=data.matrix(train[,'target']), method="lasso", preProcess="scale", trControl=control)
-# estimate variable importance
-importance <- varImp(model, scale=FALSE)
-# summarize importance
-print(importance)
+# Read in training data
+train = fread("train_final.csv", stringsAsFactors = F)
+test = fread("test_final.csv", stringsAsFactors = F)
+#stacked_col = fread("stacked_model_pred.csv", stringsAsFactors = F)
 
-# For cross-validation only (this does not train the model):
-# cv.res = xgb.cv(data= data.matrix(train[,setdiff(names(train),'target')]),
-#                 nfold = 5,
-#                 eta = 0.01,
-#                 label = train$target,
-#                 nround = 1000,
-#                 min_child_weight = 10,
-#                 max_depth = 8,
-#                 colsample_bytree = 0.8,
-#                 subsample = 0.7,
-#                 objective = "binary:logistic",
-#                 eval_metric = "auc"
-# )
+#remove first column (it is null)
+dim(train); dim(test)
+train[, V1 := NULL]; test[, V1 := NULL]
+dim(train); dim(test)
 
-model = xgboost(data= data.matrix(train[,setdiff(names(train),'target')]),
-                eta = 0.1,
-                label = train$target,
-                nround = 10,
-                #           min_child_weight = 10,
-                max_depth = 8,
-                colsample_bytree = 0.8,
-                subsample = 0.7,
-                objective = "binary:logistic",
-                eval_metric = "auc"
+## Cross Validation xgboost ###
+### Randomly split data into 5 folds ###
+### Fit xgboost into it ###
+### perform testing results ###
+for (optdep in seq(8,12,1)) {
+  for (optcldwgt in seq(6,12,1)) {
+    for (eta_i in seq(0.005,0.03,0.005)) {
+      for (nrounds_i in seq(5000,15000,2500)) {
+        res.cv <- xgb.cv(data=data.matrix(train[,setdiff(names(train),'target'), with =F]),
+                         eta = eta_i,
+                         gamma = 0,
+                         label = train$target,
+                         nrounds = nround_i,
+                         min_child_weight = 8,
+                         max_depth = 10,
+                         colsample_bytree = 0.7,
+                         subsample = 0.5,
+                         scale_pos_weight=3.3,
+                         objective = "binary:logistic",
+                         metrics = "auc",
+                         verbose = TRUE
+        )
+      }
+    }
+  }
+}
+
+eta_i=0.005
+nround_i = 18000
+
+model = xgboost(  data=data.matrix(train[,setdiff(names(train),'target'), with =F]),
+                  eta = eta_i,
+                  gamma = 0,
+                  label = train$target,
+                  nrounds = nround_i,
+                  min_child_weight = 8,
+                  max_depth = 10,
+                  colsample_bytree = 0.7,
+                  subsample = 0.5,
+                  scale_pos_weight=3.3,
+                  objective = "binary:logistic",
+                  metrics = "auc",
+                  verbose = TRUE
 )
 
 # create submission file
 submission <- data.frame(ID=testID)
 submission$target<-predict(model, newdata=data.matrix(test))
-write.csv(submission, "xgboost_submission_crap.csv",row.names = F)
+write.csv(submission, "xgboost_submission_final_Bob.csv",row.names = F)
